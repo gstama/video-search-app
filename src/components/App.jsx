6720d84902f62a87f4f5f53f52ae40b63595f6eb
gstama <gstama@outlook.com>
2021-02-16 15:04:29 +0000

    Add VideoDetail and connect it to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../api/youtube';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -16,15 +17,22 @@ class App extends React.Component {
         key: process.env.REACT_APP_GOOGLE_API_KEY,
       },
     });
-
     this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
